090252bc572f007719e5d75a0b75ce431632cd69
Musab Mahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2024-04-15 21:08:38 +0530

    Update loaderv12.js
    Signed-off-by: Musab Mahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>

--- loaderv12.js
+++ loaderv12.js
@@ -14,8 +14,8 @@
   let voiceButton;
   let mainButton;
   const client = script.getAttribute("data-client");
-  const voiceWidgetUrl = `https://neura-chat-widget.vercel.app//?avatarChat=1&client=${client}`;
-  const widgetUrl = `https://neura-chat-widget.vercel.app//?client=${client}`;
+  const voiceWidgetUrl = `https://neura-chat-widget.vercel.app/?avatarChat=1&client=${client}`;
+  const widgetUrl = `https://neura-chat-widget.vercel.app/?client=${client}`;
 
   const loadWidget = () => {
     //for chat widget
